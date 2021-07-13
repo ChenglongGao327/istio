e593ea453d05b0be5546696fceb64de5183c39f0
gaochenglong <gaochenglong@huawei.com>
2021-07-13 10:03:37 +0800

    In the scene where service entry select workload entry：set up istio endpoint port
    The priority of the model.IstioEndPoint.EndpointPort port should be: unixAddress > we.ports > se.port.targetPort > se.port.number

--- pilot/pkg/serviceregistry/serviceentry/conversion.go
+++ pilot/pkg/serviceregistry/serviceentry/conversion.go
@@ -226,14 +226,12 @@ func convertEndpoint(service *model.Service, servicePort *networking.Port,
 	endpoint *networking.WorkloadEntry, wleck *configKey) *model.ServiceInstance {
 	var instancePort uint32
 	addr := endpoint.GetAddress()
+	// priority level: unixAddress > we.ports > se.port.targetPort > se.port.number
 	if strings.HasPrefix(addr, model.UnixAddressPrefix) {
 		instancePort = 0
 		addr = strings.TrimPrefix(addr, model.UnixAddressPrefix)
-	} else if len(endpoint.Ports) > 0 { // endpoint port map takes precedence
-		instancePort = endpoint.Ports[servicePort.Name]
-		if instancePort == 0 {
-			instancePort = servicePort.Number
-		}
+	} else if port, ok := endpoint.Ports[servicePort.Name]; ok && port > 0 {
+		instancePort = port
 	} else if servicePort.TargetPort > 0 {
 		instancePort = servicePort.TargetPort
 	} else {
